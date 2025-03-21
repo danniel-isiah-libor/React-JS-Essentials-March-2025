80744e38fbafa108c96cb520a27ec0f2112534d3
Aurium29 <estelitojerome.lugtu@gmail.com>
2025-03-21 14:12:20 +0800

    add Forms

---
+++ new_react/src/Form/Field.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Field() {
+  return (
+    <div>
+      <label>Name:</label>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default Field;
---
+++ new_react/src/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import Register from "./Register";
+
+function Home() {
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <Register />
+    </>
+  );
+}
+export default Home;
