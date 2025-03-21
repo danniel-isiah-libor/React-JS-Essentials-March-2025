a4a6468cfadf6a8544c5ffa7d990ac1ff133873d
christianjavier25 <25christianjavier@gmail.com>
2025-03-21 16:08:31 +0800

    Add Tailwind CSS Framework

--- SampleProject/src/Dashboard.jsx
+++ SampleProject/src/Dashboard.jsx
@@ -15,26 +15,26 @@ export default function Dashboard() {
       body: "This is post 3",
     },
   ];
-  const styles = {
-    border: "1px solid",
-    marginBottom: "5px",
-    paddingLeft: "5px",
-    paddingRight: "5px",
-  };
+//   const styles = {
+//     border: "1px solid",
+//     marginBottom: "5px",
+//     paddingLeft: "5px",
+//     paddingRight: "5px",
+//   };
   return (
-    <>
-      <h1>Dashboard</h1>
+    <div className="p-5">
+      <h1 className="text-5xl font-bold underline">Dashboard</h1>
       <br />
-      <ul className={dashboardStyle.bgColor}>
+      <ul className="p-5 flex flex-col space-y-2">
         {posts.map((post, index) => {
           return (
-            <li style={styles}>
+            <li className="border bg-sky-200 px-3 py-1 rounded">
               <h3>{post.title}</h3>
               <p>{post.body}</p>
             </li>
           );
         })}
       </ul>
-    </>
+    </div>
   );
 }
