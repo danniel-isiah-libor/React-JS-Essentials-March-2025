3e4f91498d7b49790b727c404d05714332574cd1
Danniel Libor <dlibor.dev@gmail.com>
2025-03-21 14:08:20 +0800

    [Day 1] components

---
+++ playground/src/Register.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Field from "./form/Field.jsx"
+
+function Register() {
+  return (
+    <div>
+      <Field />
+
+      <br />
+      <br />
+
+      <Field />
+
+      <br />
+      <br />
+
+      <Field />
+
+      <br />
+      <br />
+
+      <Field />
+
+      <br />
+      <br />
+
+      <Field />
+
+      <br />
+      <br />
+
+      <button>Register</button>
+    </div>
+  );
+}
+
+export default Register;
