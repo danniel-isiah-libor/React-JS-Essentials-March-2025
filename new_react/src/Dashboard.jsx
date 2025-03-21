aed4380d17f628f984c638ad156218f1b5746dcb
Aurium29 <estelitojerome.lugtu@gmail.com>
2025-03-21 16:06:41 +0800

    add tailwind

---
+++ new_react/src/Dashboard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import dashboardStyles from "./Dashboard.module.css";
+
+function Dashboard() {
+  const posts = [
+    {
+      title: "Post 1",
+      body: "This is Post 1",
+    },
+    {
+      title: "Post 2",
+      body: "This is Post 2",
+    },
+    {
+      title: "Post 3",
+      body: "This is Post 3",
+    },
+  ];
+
+  const styles = {
+    border: "1px solid red",
+    marginBottom: "10px",
+  };
+
+  return (
+    <>
+      <div>Dashboard</div>
+      <br />
+      <ul className={dashboardStyles.bgColor}>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Dashboard;
--- new_react/src/Home.jsx
+++ new_react/src/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./Register.jsx";
+import Login from "./Login.jsx";
+import Dashboard from "./Dashboard.jsx";
 
 function Home() {
   const url = window.location.href;
@@ -10,6 +11,8 @@ function Home() {
         return <Login />;
       case "http://localhost:5173/register":
         return <Register />;
+      case "http://localhost:5173/dashboard":
+        return <Dashboard />;
       default:
         return <> Page Not Found</>;
     }
