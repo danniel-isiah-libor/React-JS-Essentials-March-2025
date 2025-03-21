474ec53004cca2f781ee67e23417b8829ffa011d
christianjavier25 <25christianjavier@gmail.com>
2025-03-21 15:56:07 +0800

    Add CSS Style

--- SampleProject/src/Dashboard.jsx
+++ SampleProject/src/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import dashboardStyle from './Dashboard.module.css'
 export default function Dashboard() {
   const posts = [
     {
@@ -15,24 +15,26 @@ export default function Dashboard() {
       body: "This is post 3",
     },
   ];
-
+  const styles = {
+    border: "1px solid",
+    marginBottom: "5px",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+  };
   return (
     <>
-    <div>Dashboard</div>
-    <br/>
-    <ul>
-       {
-         posts.map((post, index) => {
-            return (
-                <li>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-                </li> 
-            )
-         })
-       }
-
-    </ul>
+      <h1>Dashboard</h1>
+      <br />
+      <ul className={dashboardStyle.bgColor}>
+        {posts.map((post, index) => {
+          return (
+            <li style={styles}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
