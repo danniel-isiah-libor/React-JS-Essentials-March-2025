ef01dc9ff2262f87c7c633f9b290a0ff3b89bb3b
jervin <jervin.faderanga@gmail.com>
2025-03-21 14:10:44 +0800

    Up to field

---
+++ playground/src/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css'
+import Register from './Register.jsx'
+
+function Home (){
+    return (
+        <> 
+         <h1>Welcome to react</h1>
+         <Register/> 
+        </>
+
+    )
+    
+}
+
+export default Home
---
+++ playground/src/Register.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Field from "./form/Field.jsx"
+function Register() {
+  return (
+  <div>
+    <h2>Register</h2>
+    <p>Register here</p>
+    <br/>
+
+    <Field/>
+    <br/>
+    
+    <Field/>
+    <br/>
+
+    <Field/>
+    <br/>
+
+    <Field/>
+    <br/>
+
+    <Field/>
+    <br/>
+
+    <button>Register</button>
+
+    <br/>
+
+    
+  </div>
+   );
+}
+
+export default Register;
