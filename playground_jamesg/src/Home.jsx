7d97a3409b061b5e81fcce00c5ede13628f0dffb
jamesg <jamescgonzales@gmail.com>
2025-03-21 14:15:07 +0800

    register

---
+++ playground_jamesg/src/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+import Register from './Register.jsx'
+
+function Home () {  
+    return (
+        <>
+        <h1>Welcome To React</h1>
+        <p>Register here</p>
+        <Register/>
+        <br />
+
+        </>
+        
+    )    
+}
+
+export default Home
