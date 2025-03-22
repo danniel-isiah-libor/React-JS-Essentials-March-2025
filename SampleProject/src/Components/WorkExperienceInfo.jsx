4f9d6c1ba83b1a90f2b1c07b8aaf894eb59cc188
christianjavier25 <25christianjavier@gmail.com>
2025-03-22 18:11:45 +0800

    [DAY 2] useReducer

---
+++ SampleProject/src/Components/WorkExperienceInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WorkExperienceInfo() {
+  return (
+    <div>WorkExperienceInfo</div>
+  )
+}
--- SampleProject/src/Pages/Profile.jsx
+++ SampleProject/src/Pages/Profile.jsx
@@ -1,6 +1,28 @@
-import React, { useState } from 'react'
+import React, { act, useReducer, useState } from 'react'
 import Card from '../components/Card'
 
+const initialWorkExp = [
+    {
+        Company: "JX Metals",
+        Role: "Programmer",
+        Year: "Jan 2023 - March 2025"
+    },
+    {
+        Company: "Company Sample :",
+        Role: "Programmer",
+        Year: "Jan 2020 - March 2023"
+    },
+]
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'DELETE': {
+            const data = [...state]
+            data.splice(action.index, 1)
+            return data
+        }
+    }
+}
 export default function Profile() {
     const [profileInfos] = useState ([
         {
@@ -9,24 +31,29 @@ export default function Profile() {
             Email: "ChristianJavier@gmail.com"
         }
     ]) 
-    const [profileExp] = useState ([
-        {
-            Company: "JX Metals",
-            Role: "Programmer",
-            Year: "Jan 2023 - March 2025"
-        },
-        {
-            Company: "Company Sample :",
-            Role: "Programmer",
-            Year: "Jan 2020 - March 2023"
-        },
+    // const [workExp] = useState ([
+    //     {
+    //         Company: "JX Metals",
+    //         Role: "Programmer",
+    //         Year: "Jan 2023 - March 2025"
+    //     },
+    //     {
+    //         Company: "Company Sample :",
+    //         Role: "Programmer",
+    //         Year: "Jan 2020 - March 2023"
+    //     },
         
-    ])
+    // ])
+    const [workExp, dispatch] = useReducer(reducer, initialWorkExp)
+    const onDelete = (index) => {
+        dispatch({type: "DELETE", index})
+    }
+
   return (
     <div className='flex flex-col space-y-5 p-5 border max-w-[60%]'>
         
         <Card label="Profile Information" type="profile" data={profileInfos}/>
-        <Card  label="Work Experiences" type="work" data={profileExp}/>
+        <Card  label="Work Experiences" type="work" data={workExp} onDelete={onDelete}/>
 
     </div>
   )
